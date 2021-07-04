5757be508d561f8c73c25ac14a6045a7662584f9
Lais Sana <lais.sana@outlook.com>
2021-07-03 21:02:18 -0300

    Adicionando nome do integrante Vinicius

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from parserAnali import ParserAnali
 
 if __name__ == "__main__":
    lexer = Lexer('ProgramTestClass.pasc')
-   print("Grupo: { \n    Lais Sana\n    Alan Eremita\n    Leandro Gama\n}\n******Recuperação de erro implementada")
+   print("Grupo: { \n    Lais Sana\n    Alan Eremita\n    Leandro Gama\n    Vinícius Borges\n}\n******Recuperação de erro implementada")
    print("\n=>Lista de tokens:")
    parserAnali = ParserAnali(lexer)
 
